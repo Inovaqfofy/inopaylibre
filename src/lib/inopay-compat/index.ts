e34941f77cdf4c1cb6790b06e73184535286809f
Youssouf Fofana <fofanay@gmail.com>
2025-12-31 18:59:09 -0500

    Suppression de tout les fichiers

--- src/lib/inopay-compat/index.ts
+++
@@ -1,7 +0,0 @@
-// Inopay Compatibility Layer
-// Auto-generated polyfills for sovereign code
-export * from './use-auth';
-export * from './supabase-client';
-export * from './use-toast';
-export * from './use-mobile';
-export * from './use-sidebar';
